d477ef4863c698be6fa98992cfab0feb683df455
imadaqel <emadaqel92@gmail.com>
2020-11-03 14:57:26 +0200

    oh noo

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+var path = require('path');
+
+var app = express();
+var PORT = 3000; 
 
 
+
+
+
+
+app.listen(PORT, () => {
+    console.log('hhhhhhhh')
+  });
+app.get('/', (req, res) => { 
+   // res.sendfil("POST Request Called") 
+    res.sendFile(path.join(__dirname,"/client/index.html"))
+    
+  }) 
+
+
+  app.post('/', (req, res) => { 
+   console.log('hiiiii')
+  }) 
